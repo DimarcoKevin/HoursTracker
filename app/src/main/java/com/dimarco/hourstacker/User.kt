84e17e115ff65e563bb1cac53edb4c0b07686060
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2020-07-07 16:32:22 -0400

    Changing birthday to a date instead of string

--- app/src/main/java/com/dimarco/hourstacker/User.kt
+++ app/src/main/java/com/dimarco/hourstacker/User.kt
@@ -1,16 +1,18 @@
 package com.dimarco.hourstacker
 
+import java.util.*
+
 class User {
     private var id: Int = 0
     private var name: String = ""
-    private var birthday: String = ""
+    private var birthday: Date? = null
 
 
     constructor(name: String) {
         this.name = name
     }
 
-    constructor(name: String, birthday: String) {
+    constructor(name: String, birthday: Date) {
         this.name = name
         this.birthday = birthday
     }
