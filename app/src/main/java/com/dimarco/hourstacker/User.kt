1e3239d31c12f7e9a184b478e36935948175d6f3
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2020-07-07 16:27:57 -0400

    Adding User class

--- app/src/main/java/com/dimarco/hourstacker/User.kt
+++ app/src/main/java/com/dimarco/hourstacker/User.kt
@@ -1,2 +1,18 @@
 package com.dimarco.hourstacker
 
+class User {
+    private var id: Int = 0
+    private var name: String = ""
+    private var birthday: String = ""
+
+
+    constructor(name: String) {
+        this.name = name
+    }
+
+    constructor(name: String, birthday: String) {
+        this.name = name
+        this.birthday = birthday
+    }
+
+}
