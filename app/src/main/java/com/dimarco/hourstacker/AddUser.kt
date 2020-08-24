818a1f311876da457b3c0b7558f7cc0d3071df6c
Kevin <kevin.j.dimarco@gmail.com>
2020-08-23 23:14:59 -0700

    Adding edit by ID for users

--- app/src/main/java/com/dimarco/hourstacker/AddUser.kt
+++ app/src/main/java/com/dimarco/hourstacker/AddUser.kt
@@ -3,6 +3,7 @@ package com.dimarco.hourstacker
 import android.content.ContentValues
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.create_user.*
 import java.lang.Exception
@@ -39,8 +40,26 @@ class AddUser : AppCompatActivity() {
         var userDbManager = UserDbManager(this)
         var values = ContentValues()
         values.put(user, txtName.text.toString())
-        
 
+        if (id == 0) {
+            val id = userDbManager.insert(values)
+
+            if (id > 0) {
+                Toast.makeText(this, "User has been added.", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Cannot add user.", Toast.LENGTH_LONG).show()
+            }
+
+        } else {
+            var selectionArgs = arrayOf(id.toString())
+            userDbManager.edit(values, "ID=?", selectionArgs)
+
+            if (id > 0) {
+                Toast.makeText(this, "User has been edited.", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Cannot edit user.", Toast.LENGTH_LONG).show()
+            }
+        }
+        finish()
     }
-
 }
