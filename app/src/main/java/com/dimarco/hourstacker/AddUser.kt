fddbba43127c293ed593340e8a023004e6718fd8
Kevin <kevin.j.dimarco@gmail.com>
2020-08-23 22:26:47 -0700

    Creating AddUser class and adding OnCreate method

--- app/src/main/java/com/dimarco/hourstacker/AddUser.kt
+++ app/src/main/java/com/dimarco/hourstacker/AddUser.kt
@@ -1,2 +1,27 @@
 package com.dimarco.hourstacker
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.create_user.*
+import java.lang.Exception
+
+class AddUser : AppCompatActivity() {
+    /**
+     * creating private values for each column in the User table
+     */
+    private val user = "User"
+    private var id = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_user)
+
+        var bundle= intent.extras
+        try {
+            id=bundle!!.getInt("ID")
+            txtName.setText(bundle.getString("User"))
+        } catch(ex: Exception) {
+            ex.printStackTrace()
+        }
+    }
+}
